88b4d0a39289754ae47d8426edb8f5217309ae29
Chetan Patil <chetanpatil2351@gmail.com>
2025-07-14 20:41:28 +0530

    Added sample_ifelse.cpp file

---
+++ sample_ifelse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+   int age;
+   cin>> age;
+   cout<< "enter your age" <<endl;
+   if(age>18){
+    cout<<"eligible for voting"<<endl;
+    }
+    else{
+        cout<< "not eligible"<<endl;
+    }
+    return 0;
+   
+}
